24466da27096d697271f0bdafd14433b2e4337cc
nancyabcec1058 <nancyec1058@gmail.com>
2026-08-24 08:16:00 +0530

    Refactor formParams to formParam for access token

--- src/OAuthCourses.java
+++ src/OAuthCourses.java
@@ -10,8 +10,8 @@ public class OAuthCourses {
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		
 		//Get access_token
-		String response=given().formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
-		.formParams("client_secret","erZOWM9g3UtwNRj340YYaK_W").formParams("grant_type","client_credentials").formParams("scope","trust")
+		String response=given().formParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+		.formParam("client_secret","erZOWM9g3UtwNRj340YYaK_W").formParam("grant_type","client_credentials").formParam("scope","trust")
 		.when().log().all().post("/oauthapi/oauth2/resourceOwner/token").then().log().all().assertThat().statusCode(200).extract().response().asString();
 		JsonPath js=new JsonPath(response);
 		String accessToken=js.getString("access_token");
